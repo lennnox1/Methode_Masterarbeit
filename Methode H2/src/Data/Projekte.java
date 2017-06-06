4ef7ecf3b42b9c8ce9c369c77edf6d9348ac77a7
Lennnox1 <pvenzke@aol.com>
2017-06-06 16:44:02 +0200

    TEST

---
+++ Methode H2/src/Data/Projekte.java
@@ -0,0 +1,8 @@
+package Data;
+
+public class Projekte {
+	public String Projekt_name;
+	public int idProjekte;
+	public int Anz_Montageop;
+	public int idKriterienkataloge;
+}
---
+++ Methode H2/src/Data/Used_auspr.java
@@ -0,0 +1,11 @@
+package Data;
+
+public class Used_auspr {
+		public int idused_Auspr;
+		public int idMontOP;
+		public int idAuspr;
+		public double ratingFM;
+		public double ratingFR;
+		public boolean relevant;
+		public int gewichtung;
+}
